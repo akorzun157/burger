483d7bd7dae0503591e0a3e6ad31e0164bc5742f
Andrey Korzun <akorzun157@gmail.com>
2024-11-30 13:25:04 +0300

    button for choose category was added

--- menu/views.py
+++ menu/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from menu.models import Products
+from menu.models import Categories, Products
 
 # Create your views here.
 def menu(request):
 
     menu = Products.objects.all()
+    categories = Categories.objects.all()
 
     context = {
         'title': 'Burger - Меню',
         'menu': menu,
+        'categories': categories,
     }
     return render(request, 'menu/menu.html', context)
